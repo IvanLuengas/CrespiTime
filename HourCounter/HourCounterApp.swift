02a8cf78e628fdab773ea875bbbc97c53b6cae15
IvanLuengas <ing.ivanluengas@hotmail.com>
2024-11-13 19:08:22 +1100

    Initial Commit

---
+++ HourCounter/HourCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourCounterApp.swift
+//  HourCounter
+//
+//  Created by Ivan Luengas on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HourCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HourCounterTests/HourCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  HourCounterTests.swift
+//  HourCounterTests
+//
+//  Created by Ivan Luengas on 13/11/24.
+//
+
+import Testing
+@testable import HourCounter
+
+struct HourCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
